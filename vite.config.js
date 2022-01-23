089c991d0bd82dab600988146b2e1ff219d96828
mstanka <stmmarketa@gmail.com>
2022-01-23 15:40:57 +0100

    WIP stylesheet link

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,20 @@ export default defineConfig({
 	server: {
 		port: 3030,
 	},
+	build: {
+		rollupOptions: {
+		  external: ['/dist/assets/index.css'],
+		} 
+	},
+	css: {
+        modules: {
+			localsConvention: 'camelCase',
+            scopeBehaviour: 'global'
+        },
+		preprocessorOptions: {
+			style: {
+			  imports: [path.resolve(__dirname, '/index.css')]
+			}
+		 }
+    }
 });
