e3629e27715099f66bbd31a5999190422a6eb2eb
mstanka <stmmarketa@gmail.com>
2022-01-23 18:25:41 +0100

    changes in config to fix stylesheet

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3030,
-	},
-	build: {
-		rollupOptions: {
-			external: ['/dist/assets/index.css'],
-		},
-	},
-	css: {
-		modules: {
-			localsConvention: 'camelCase',
-			scopeBehaviour: 'global',
-		},
-		preprocessorOptions: {
-			style: {
-				imports: [path.resolve(__dirname, '/index.css')],
-			},
-		},
 	},
 });
